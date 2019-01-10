2fdaac77b7fcc48207896761b174eb7f52237456
Tilman Raendchen <TRaendchen@outlook.com>
2019-01-10 23:02:48 +0000

    Second Working Iteration
    Includes working command module system and client-side websocket integration (won't work without server-side integration).

---
+++ client/index.js
@@ -0,0 +1,3 @@
+// A shorthand module for easy reference to the discord client object
+const Client = require("../lib/classes/Client.js");
+module.exports = new Client();
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// declare a global var that holds the root path
-global.__base = __dirname;
-
 // populate the `process.env` object from local `.env` file
 require("dotenv").load();
-const join = require("path").join
-    , Client = require("./lib/classes/Client.js");
 
-const client = new Client();
-client.once("ready", require(join(__base, "lib", "handlers", "ready.js")));
+const client = require("./client");
+client.once("ready", require(`${client.Client.__paths.handlers}/ready.js`));
 
 client.login(process.env.BOT_TOKEN)
     .catch(console.error);
