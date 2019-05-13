a8b073edc2fdbacb9481543cfb29554bc10773b9
Tilman Raendchen <TRaendchen@outlook.com>
2019-05-13 02:45:06 +0100

    Rework base module execution + other progress

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // populate the `process.env` object from local `.env` file
 require("dotenv").load();
 
+global["__base"] = process.cwd();
+
 const client = require("./client");
 client.once("ready", require(`${client.Client.__paths.handlers}/ready.js`));
 
