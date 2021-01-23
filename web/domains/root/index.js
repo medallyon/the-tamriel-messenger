f679952f64f33a942becb610d417e5839d5d9fc9
Tilman Raendchen <traendchen@outlook.com>
2021-01-23 21:38:20 +0000

    Updates

--- web/domains/root/index.js
+++ web/domains/root/index.js
@@ -5,15 +5,19 @@ const router = require("express")()
 router.set("views", join(__dirname, "views"));
 router.set("view engine", "ejs");
 
-// eslint-disable-next-line
-router.get("/", (req, res) => { res.redirect("/discord"); });
 router.get("/discord", function(req, res)
 {
 	res.redirect("https://discord.gg/esoi");
 });
 
+router.get("/", (req, res) =>
+{
+	res.redirect("/home");
+});
+
 router.use("/admin", require(join(__dirname, "admin")));
 
+// eslint-disable-next-line no-useless-escape
 router.use("/:shortLinkID([a-zA-Z0-9\._-]+)", function(req, res, next)
 {
 	const urlPath = join(__basedir, "data", "admin", "shortLinks.json");
