d906338ad2acc1c2b47ba08b6435ab26b0d10c32
Tilman Raendchen <traendchen@outlook.com>
2021-01-24 21:53:47 +0000

    Rework middleware to be sliccer

--- web/middleware/admin/userIsAdmin.js
+++ web/middleware/admin/userIsAdmin.js
@@ -1,7 +1,8 @@
-module.exports = function(req, res, next)
+module.exports = async function(req, res, next)
 {
-	const { client } = require(__basedir, "index.js")
-		, guild = client.guilds.cache.get("130716876937494528")
+	const { client } = require(__basedir, "index.js");
+
+	const guild = await client.guilds.fetch("130716876937494528")
 		, guards = guild.members.cache.filter(x => x.roles.cache.has("598409804465045534"));
 
 	if (!guards.has(req.user.id))
