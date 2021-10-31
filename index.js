4e53241860d40a96f2880fc29824515d763a14f7
Tilman Raendchen <tilman@medallyon.me>
2021-10-31 12:57:08 +0000

    d.jsv13 upgrade & cleanup

--- index.js
+++ index.js
@@ -1,56 +1,20 @@
-require("dotenv").config();
+require("./globals.js");
 
-global.join = require("path").join;
-global.__basedir = __dirname;
-global.__clientdir = join(__dirname, "lib");
-global.__webdir = join(__dirname, "web");
+require("dotenv").config({
+	path: (process.argv[3] || process.env.NODE_ENV || "").toLowerCase().includes("prod")
+		? join(__dirname, ".env")
+		: join(__dirname, ".env.dev")
+});
 
-const Client = require("./lib/classes/DiscordClient.js");
-let client = new Client();
 
-client.login((process.env.NODE_ENV || "").toLowerCase() === "production" ? process.env.BOT_TOKEN : process.env.BOT_TOKEN_DEV)
+const DiscordClient = require(join(__libdir, "classes", "DiscordClient.js"));
+const client = new DiscordClient();
+
+client.login(process.env.BOT_TOKEN)
 	.catch(console.error);
 
-const web = require("./web");
+const web = require(__webdir);
 
-const { GuildMember } = require("discord.js");
-client.ws.on("INTERACTION_CREATE", async interaction =>
-{
-	let channel, member, target;
-	try
-	{
-		channel = await client.channels.fetch(interaction.channel_id);
-		member = new GuildMember(client, interaction.member, channel.guild);
-		target = await channel.guild.members.fetch(interaction.data.options[0].value);
-	}
-
-	catch (err)
-	{
-		console.error(err);
-	}
-
-	try
-	{
-		client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 4,
-				data: {
-					content: "",
-					embeds: [
-						client.commands.whoIs.run({
-							guild: channel.guild,
-							member
-						}, target || member).toJSON()
-					]
-				}
-			}
-		});
-	}
-
-	catch (err)
-	{
-		console.error(err);
-	}
 });
 
 module.exports = { client, web };
